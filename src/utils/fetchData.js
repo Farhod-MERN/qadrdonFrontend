d102c7f17464bf3f91f936f0bd0ae8c2dee533ed
Farhod <mrfarhod58@gmail.com>
2023-04-02 22:04:54 +0500

    fetch data

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,36 +1,42 @@
 import axios from 'axios'
 
 export const getDataAPI = async (url, token) => {
-    const res = await axios.get(`/api/${url}`, {
+    const res = await axios.get(`https://qadrdon-uz.onrender.com/api/${url}`, {
         headers: { Authorization: token}
     })
     return res;
 }
 
 export const postDataAPI = async (url, post, token) => {
-    const res = await axios.post(`/api/${url}`, post, {
+    const res = await axios.post(`https://qadrdon-uz.onrender.com/api/${url}`, post, {
         headers: { Authorization: token}
     })
     return res;
 }
 
 export const putDataAPI = async (url, post, token) => {
-    const res = await axios.put(`/api/${url}`, post, {
+    const res = await axios.put(`https://qadrdon-uz.onrender.com/api/${url}`, post, {
         headers: { Authorization: token}
     })
     return res;
 }
 
 export const patchDataAPI = async (url, post, token) => {
-    const res = await axios.patch(`/api/${url}`, post, {
+    const res = await axios.patch(`https://qadrdon-uz.onrender.com/api/${url}`, post, {
         headers: { Authorization: token}
     })
     return res;
 }
 
 export const deleteDataAPI = async (url, token) => {
-    const res = await axios.delete(`/api/${url}`, {
+    const res = await axios.delete(`https://qadrdon-uz.onrender.com/api/${url}`, {
         headers: { Authorization: token}
     })
     return res;
 }
+// export const deleteDataAPI = async (url, token) => {
+//     const res = await axios.delete(`/api/${url}`, {
+//         headers: { Authorization: token}
+//     })
+//     return res;
+// }
